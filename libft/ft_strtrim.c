0bf8f27d2775004cdb469fe56f78f754562c48f9
s-henderson14 <106280033+s-henderson14@users.noreply.github.com>
2022-10-29 17:24:10 +0200

    Add files via upload

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -6,38 +6,34 @@
 /*   By: shenders <marvin@codam.nl>                   +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/10/24 10:57:40 by shenders      #+#    #+#                 */
-/*   Updated: 2022/10/24 14:57:32 by shenders      ########   odam.nl         */
+/*   Updated: 2022/10/29 17:23:22 by shenders      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include "libft.h"
+#include <stdlib.h>
 
 char	*ft_strtrim(char const *s1, char const *set);
 
 char	*ft_strtrim(char const *s1, char const *set)
-{	
-	unsigned int	i;
-	char		*start;
-	char		*end;
-	
-	i = 0;
-	start = (char *)&s1[i];
-	end =  (char *)&s1[ft_strlen(s1)] - 1; 
-	if (!s1)
+{
+	unsigned int	start;
+	char			*buff;
+
+	start = 0;
+	if (!s1 || !set)
 		return (0);
-	if (!set)
-		return ((char *)s1);
-	while ( start && end && i <= ft_strlen(set))
-	{	
-		if (&s1[i] == start)
-			start[i++];
-		else if (&s1[i] == end)
-			end[i--];
-		if ((!i) > 0 || (!i) < 0)
-		return ((char *)s1 + i);
-	}
-	return (0);
+	buff = (char *) malloc(len * sizeof(char) + 1);
+	if (!buff)
+		return (0);
+	buff = (char *) s1;
+	while (*buff && ft_strchr(set, *buff))
+		buff++;
+	len = ft_strlen(buff);	
+	while (*(buff + len) && ft_strrchr(set, *(buff + len)))
+		len--;
+	return (ft_substr(buff, 0, len + 1));
 }
 
 /*int	main()
