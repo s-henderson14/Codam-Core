c6d6af69bbdc12fd6f43f35fd78de5208d22b92e
s-henderson14 <106280033+s-henderson14@users.noreply.github.com>
2022-11-03 09:18:40 +0100

    Add files via upload

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -6,35 +6,42 @@
 /*   By: shenders <marvin@codam.nl>                   +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/10/24 10:57:40 by shenders      #+#    #+#                 */
-/*   Updated: 2022/10/24 14:57:32 by shenders      ########   odam.nl         */
+/*   Updated: 2022/11/03 09:17:51 by shenders      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
-
 #include <stdio.h>
 #include "libft.h"
+#include <stdlib.h>
 
 char	*ft_strtrim(char const *s1, char const *set);
 
 char	*ft_strtrim(char const *s1, char const *set)
-{	
-	size_t	i;
-	size_t	len;
-	char	*str;
+{
+	size_t		len;
+	size_t		i;
+	size_t		j;
+	char		*str;
 
 	i = 0;
+	if (s1[i] == '\0')
+		return (ft_strdup(""));
 	while (ft_strchr(set, s1[i]))
 		i++;
+	j = 0;
 	len = ft_strlen(s1);
 	while (i < len && ft_strrchr(set, s1[len - 1]))
+	{	
 		len--;
+		j++;
+	}
 	str = ft_substr(s1, i, len - i);
 	return (str);
 }
 
 /*int	main()
 {
-	char	s1[] = "Hello";
-	char	set[] = "lo";
+	char	s1[] = "";
+	char	set[] = "";
 
 	printf("%s", ft_strtrim(s1, set));
 }*/
