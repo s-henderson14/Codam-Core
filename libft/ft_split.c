3a899d0d66e67a050e8df376eaeb6f8c201aea51
s-henderson14 <106280033+s-henderson14@users.noreply.github.com>
2022-11-03 14:35:01 +0100

    Add files via upload

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -1,53 +1,84 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ft_split.c                                         :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: shenders <marvin@codam.nl>                   +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2022/11/03 08:55:41 by shenders      #+#    #+#                 */
+/*   Updated: 2022/11/03 14:33:50 by shenders      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdio.h>
 #include "libft.h"
+#include <stdlib.h>
+
+int	ft_occur(const char *str, char c)
+{
+	int	count;
+	int	i;
+	
+	i =0;
+	count = 0;
+	while ( str[i] && str[i] == c)
+	i++;
+	while(str[i] && str[i] != c)
+	{	
+		if(str[i] != c && str[i + 1] == c)
+			count++;
+		i++;
+	}
+	return (count);
+}	
 
 char	**ft_split(char const *s, char c);
 
 char	**ft_split(char const *s, char c)
 {
-	//create temp char pointer "temp"
-	//create char pointer "str"
-	//create int index "i"
-	//iterate through s
-	//if char in 's' is equal to char 'c'
-	//make c equal to a '\0'
-	//take index of when 's' == 'c'
-	//use index to malloc correct size - sizeof(char) * (len + 1)
-	//protect malloc
-	//make "temp" == "str"
-	//free "str"
-	//how to loop round >> malloc >> add malloc str to temp and create enough buffer size for it
 	char	**temp;
 	char	*str;
 	int	i;
 	int	j;
+	int	start;
 
+	start = 0;
 	i = 0;
 	j = 0;
 	str = (char *)s;
-	temp = (char **)str;
-	while (str[i])
+	temp = (char **) malloc(sizeof(char *) * ft_occur(s, c) + 1);
+	if (!temp)
+		return(0);
+	while (s[i])
 	{	
-		if ( str[i] == c)
-		{       
-			temp[j] = ft_substr(str, 0, i);
-			if (str)
-				free(str);
+		if ( s[i] == c)
+		{
+			temp[j] = ft_substr((char *)s, start, i);
+			start = i + 1;
 			j++;
-			i++;
 		}
 		i++;	
 	}
 	if (temp)
+	{	
+		temp[ft_occur(s, c) + 1] = NULL;	
 		return (temp);
-	return (char **) str;	
+	}
+	return (0);	
 
 }
 
 /*int	main()
 {
-	char	string[] = "Sean was not yet sure how the function worked";
-	char	delimta = " ";
+	int	i;
+	char **arr;
 
-	printf("%p", ft_split(string, delimta);
-}*/
+	i = 0;
+	arr = ft_split("Sean was not yet sure how the function worked", ' ');
+	while (arr[i])
+	{
+		printf("%s\n", arr[i]);
+		i++;
+	}
+	printf("%d\n", i);
+}*/	
