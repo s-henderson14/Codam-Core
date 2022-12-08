454b66b33b5caf99fb81b4c38a599ddf98e9b0cd
s-henderson14 <106280033+s-henderson14@users.noreply.github.com>
2022-12-08 16:27:43 +0100

    Add files via upload

--- get_next_line/readfile.c
+++ get_next_line/readfile.c
@@ -6,7 +6,7 @@
 /*   By: shenders <shenders@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/08 14:38:11 by shenders          #+#    #+#             */
-/*   Updated: 2022/12/08 15:43:41 by shenders         ###   ########.fr       */
+/*   Updated: 2022/12/08 16:26:12 by shenders         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,29 @@
 
 //#define BUFFER_SIZE 10
 
+char	find_new_line(char *text, int c)
+{
+	int	i;
+
+	i = 0;
+	if (!text[i])
+		return (-1);
+	while (text[i] != '\0')
+	{	
+		if (text[i] == (char)c)
+			return (text[i]);
+		i++;
+	}
+	if (text[i] == '\0')
+		return (0);			
+				
+}
+
 char	*readfile (int fd)
 {	
 	//static char buf[BUFFER_SIZE + 1]
 	static char	buf[212];
-	int		i;
-	i = 0;
+	
 	if (fd == -1)
 		return (NULL);
 	if (fd != EOF)	
@@ -40,4 +57,5 @@ int	main(void)
 	if (fd == (-1))
 		return (-1);
 	printf("%s", readfile(fd));
+	close(fd);
 }
