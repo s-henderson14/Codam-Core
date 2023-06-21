5388d46ff48559a1df63a65a931a624dee3dca2e
s-henderson14 <106280033+s-henderson14@users.noreply.github.com>
2023-06-21 09:08:10 +0200

    Add files via upload

--- Fdf/draw_line.c
+++ Fdf/draw_line.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   draw_line.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: shenders <shenders@student.42.fr>          +#+  +:+       +#+        */
+/*   By: sean <sean@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/12 07:50:46 by shenders          #+#    #+#             */
-/*   Updated: 2023/06/16 17:05:54 by shenders         ###   ########.fr       */
+/*   Updated: 2023/06/21 09:04:23 by sean             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,41 +21,43 @@ void    draw_line(int x0, int y0, int x1, int y1, t_map *map)
     int z0;
     int z1;
 
-    z0 = map->points[(int)y0][(int)x0]; // Creating points to describe altitude taken from 2D array. 
+    z0 = map->points[(int)y0][(int)x0];
     z1 = map->points[(int)y1][(int)x1];
 
+    printf("z0 = %d, z1 = %d\n", z0, z1);
+    
     x0 -= map->width / 2;
     y0 -= map->height / 2;
     x1 -= map->width / 2;
-    y1 -= map->height / 2;
-    
-    map->scale = 1 * ((HEIGHT / 2) / map->height);
+    y1 -= map->height / 2; 
+    map->scale = (HEIGHT / 4) / map->height;
     x0 *= map->scale;
-    x1 *= map->scale;
     y0 *= map->scale;
+    x1 *= map->scale;
     y1 *= map->scale;
-    z0 *= map->scale;
-    z1 *= map->scale;
-    
-    make_iso(&x0, &y0, z0 / map->height);
-    make_iso(&x1, &y1, z1 / map->height);
-    
+    // z0 *= map->scale;
+    // z1 *= map->scale;
+    make_iso(&x0, &y0, z0);
+    make_iso(&x1, &y1, z1);
+
     if (z0 || z1 > 0)
-        map->colour = 0xFF9EF7; // Red color for higher elevation
+        map->colour = 0xdb4d69; // Red color for higher elevation
     else
-        map->colour = 0xFFFFFF; // White color for lower elevation
+        map->colour = 0xFFFFFF;
+     // White color for lower elevation
 
     x0 += WIDTH / 2;
     y0 += HEIGHT / 2;
     x1 += WIDTH / 2;
     y1 += HEIGHT / 2;
-
+    printf("x0 = %d, y1 = %d\n", x0, y0);
+    printf("x1 = %d, y1 = %d\n", x1, y1);
     x_diff = ft_abs(x1 - x0);
     y_diff = ft_abs(y1 - y0);
     error = x_diff - y_diff;
     while (x0 != x1 || y0 != y1)
     {   
-        if (x0 < (int)map->img->width && x0 > 0 && y0 < (int)map->img->height && y0 > 0)
+        if (x0 <= (int)map->img->width && x0 > 0 && y0 <= (int)map->img->height && y0 > 0)
             mlx_put_pixel(map->img, x0, y0, map->colour);
         e2 = error * 2;
         if (e2 > -y_diff)
--- Fdf/mlx_start.c
+++ Fdf/mlx_start.c
@@ -1,9 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mlx_start.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sean <sean@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/04 22:17:24 by sean              #+#    #+#             */
+/*   Updated: 2023/06/20 20:21:02 by sean             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
 #include "../../fdf.h"
 
 void	mlx_start(t_map *map)
 {
 	map->mlx = mlx_init(WIDTH, HEIGHT, "Fdf", false);
 	map->img = mlx_new_image(map->mlx, WIDTH, HEIGHT);
+	//map->scale = 4;
 	render_map(map);
 	mlx_image_to_window(map->mlx,map->img, 0, 0);
 	mlx_loop_hook(map->mlx, &esc_key_hook, map);
