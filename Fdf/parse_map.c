451990844e20a68dd052a230a99bc153e29b10d2
s-henderson14 <106280033+s-henderson14@users.noreply.github.com>
2023-06-28 09:58:50 +0200

    Add files via upload

--- Fdf/parse_map.c
+++ Fdf/parse_map.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parse_map.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: shenders <shenders@student.42.fr>          +#+  +:+       +#+        */
+/*   By: sean <sean@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/18 12:58:19 by shenders          #+#    #+#             */
-/*   Updated: 2023/06/25 18:32:01 by shenders         ###   ########.fr       */
+/*   Updated: 2023/06/27 12:30:05 by sean             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,16 +49,10 @@ int	find_width(char *map)
 	i = 0;
 	row = get_next_line(fd, 1);
 	if (!row)
-	{	
-		close(fd);
-		return (-1);
-	}
+		return (close(fd), -1);
 	args = ft_split(row, ' ');
 	if (!args)
-	{	
-		close(fd);
-		return (free(row), -1);
-	}
+		return (close(fd), free(row), -1);
 	while (args[i] && *args[i] != 10)
 	{
 		width++;
@@ -66,26 +60,25 @@ int	find_width(char *map)
 	}
 	free(row);
 	clean_split(args);
-	close(fd);
-	return (width);
+	return (close(fd), width);
 }
 
-
-
-void	save_point(t_map *map, int *point, char *row)
+void	save_point(t_map *map, int y, char *row)
 {
 	char	**coordinate;
-	int		i;
+	int		x;
 
 	coordinate = ft_split(row, ' ');
 	if (!coordinate)
 		clean_error(map, map->points);
-	i = 0;
-	while (coordinate[i])
-	{
-		point[i] = ft_atoi(coordinate[i]);
-		free(coordinate[i]);
-		i++;
+	x = 0;
+	while (x < map->width)
+	{	
+		map->points[y][x].x = x;
+		map->points[y][x].y = y;
+		map->points[y][x].z = ft_atoi(coordinate[x]);
+		free(coordinate[x]);
+		x++;
 	}
 	free(coordinate);
 }
@@ -93,44 +86,47 @@ void	save_point(t_map *map, int *point, char *row)
 void	add_points(t_map *map, char *file)
 {
 	int		fd;
-	int		i;
 	char	*line;
+	int		y;
 
 	fd = open(file, O_RDONLY, 0);
 	if (!fd)
 		clean_error(map, map->points);
-	i = 0;
 	line = get_next_line(fd, 0);
 	if (!line)
 	{	
 		close(fd);
 		clean_error(map, map->points);
 	}
-	while (line)
+	y = 0;
+	while (y < map->height)
 	{
-		save_point(map, map->points[i++], line);
+		save_point(map, y, line);
 		free(line);
 		line = get_next_line(fd, 0);
+		y++;
 	}
-	free(line);
+	if (line)
+		free(line);
 	close(fd);
 }
 
 void	parse_map(t_map *map, char *file)
 {
-	int		i;
+	int	i;
 
 	map->height = find_height(file);
 	map->width = find_width(file);
-	map->points = (int **) ft_calloc((map->height), sizeof (int *));
+	map->points = ft_calloc(map->height, sizeof (t_point *));
 	if (map->height == -1 || map->width == -1 || !map->points)
 		clean_error(map, map->points);
 	i = 0;
 	while (i < map->height)
 	{
-		map->points[i++] = (int *)ft_calloc((map->width), sizeof(int));
-		if (!map->points[0])
+		map->points[i] = ft_calloc((map->width), sizeof(t_point));
+		if (!map->points[i])
 			clean_error(map, map->points);
+		i++;
 	}
 	add_points(map, file);
 }
